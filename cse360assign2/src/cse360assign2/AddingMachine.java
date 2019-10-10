8677ebd6f7cce7d93c71484c272b0dd5366ea39b
Cody Brock <crbrock@asu.edu>
2019-10-09 22:02:35 -0700

    Wrote intended functionality for methods, added history variable, updated documentation and produced javadocs.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,61 +1,86 @@
-/***************************************
+/******************************************************************************
 * Cody Brock
 * CSE 360
 * Assignment #2
+* https://github.com/codybrock/cse360assign2
 * ---
 * AddingMachine
-* Description of file contents
-***************************************/
+* ---
+* A simple class that allows the user to add and subtract from a running
+* integer total. Starting from zero, the program keeps track of addition
+* and subtraction operations and can return the current total as an integer
+* or show a complete history of operations used as a formatted string.
+******************************************************************************/
 package cse360assign2;
 
+/**
+ * Object stores an integer total and a history of operations as a String.
+ * Contains methods for adding and subtracting integers from the total, as
+ * well as returning the total or the history string. Also has a clear method
+ * to reset the total and history variables, essentially re-initializing the
+ * object.
+ */
 public class AddingMachine {
 
 	private int total;
+	private String history;
 	
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+		total = 0;
+		history = "0";
 	}
 	
 	/**
-	 * Description of getTotal function  
+	 * Returns the total variable
 	 * 
-	 * @return      description
+	 * @return		the total variable	
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
-	 * Description of add function
+	 * Adds a passed value to total
+	 * <p>
+	 * Also modifies the history variable to reflect the change.
 	 * 
-	 * @param  int  description
+	 * @param  value  value to be added to the total
 	 */
 	public void add (int value) {
-		
+		total += value;
+		history += " + " + value;
 	}
 	
 	/**
-	 * Description of add subtract
+	 * Subtracts a passed value from total
+	 * <p>
+	 * Also modifies the history variable to reflect the change.
 	 * 
-	 * @param  int  description
+	 * @param  value  value to be subtracted from total
 	 */
 	public void subtract (int value) {
-		
+		total -= value;
+		history += " - " + value;
 	}
 	
 	/**
-	 * Description of toString function
+	 * Returns an accounting of how the total has been modified in the form
+	 * of a String.
+	 * <p>
+	 * (ie "1 + 3 - 2 + 4")
 	 * 
-	 * @return      description
+	 * @return      Operation history in String form
 	 */
 	public String toString () {
-		return "";
+		return history;
 	}
 	
 	/**
-	 * Description of clear function
+	 * Resets the program, returning the total variable to 0 and the
+	 * history variable to "0".
 	 */
 	public void clear() {
-	
+		total = 0;
+		history = "0";	
 	}
 }

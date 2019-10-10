c15fc8c032f7002610f8f21585a062506756c6c1
Cody Brock <crbrock@asu.edu>
2019-10-09 21:12:15 -0700

    Initial commit

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,61 @@
+/***************************************
+* Cody Brock
+* CSE 360
+* Assignment #2
+* ---
+* AddingMachine
+* Description of file contents
+***************************************/
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Description of getTotal function  
+	 * 
+	 * @return      description
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Description of add function
+	 * 
+	 * @param  int  description
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Description of add subtract
+	 * 
+	 * @param  int  description
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Description of toString function
+	 * 
+	 * @return      description
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * Description of clear function
+	 */
+	public void clear() {
+	
+	}
+}
